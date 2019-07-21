d08971c72d0ce25d8118058228fa600d2cbbd061
bdange <benjamin.dange@gmail.com>
2019-07-21 15:49:40 +0200

    fixing routes

---
+++ spec/integration/users_spec.js
@@ -0,0 +1,83 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/users/";
+const User = require("../../src/db/models").User;
+const sequelize = require("../../src/db/models/index").sequelize;
+
+describe("routes : users", () => {
+  beforeEach(done => {
+    sequelize
+      .sync({ force: true })
+      .then(() => {
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+
+  describe("GET /users/sign_up", () => {
+    it("should render a view with a sign up form", done => {
+      request.get(`${base}sign_up`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Welcome to Blocipedia");
+        done();
+      });
+    });
+  });
+
+  describe("POST /users/sign_up", () => {
+    // #1
+    it("should create a new user with valid values and redirect", done => {
+      const options = {
+        url: base,
+        form: {
+          username: "user_name",
+          email: "user@example.com",
+          password: "123456789"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+        // #2
+        User.findOne({ where: { email: "user@example.com" } })
+          .then(user => {
+            expect(user.username).toBe("user_name");
+            expect(user.email).toBe("user@example.com");
+            expect(user.id).toBe(1);
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+      });
+    });
+
+    // #3
+    it("should not create a new user with invalid attributes and redirect", done => {
+      request.post(
+        {
+          url: `${base}sign-up`,
+          form: {
+            username: "wrong-name",
+            email: "no",
+            password: "123456789"
+          }
+        },
+        (err, res, body) => {
+          User.findOne({ where: { email: "no" } })
+            .then(user => {
+              expect(user).toBeNull();
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        }
+      );
+    });
+  });
+});
