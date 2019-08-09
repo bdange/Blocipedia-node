229478c4127593cb5e2c77f8347048f53be7a00e
bdange <benjamin.dange@gmail.com>
2019-08-09 23:00:15 +0200

    updating queries.users

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -4,28 +4,25 @@ const bcrypt = require("bcryptjs");
 //sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
-  // #2
   createUser(newUser, callback) {
-    // #3
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
 
-    // #4
     return User.create({
       username: newUser.username,
       email: newUser.email,
       password: hashedPassword
     })
       .then(user => {
-        const msg = {
-          to: newUser.email,
-          from: "test@example.com",
-          subject: "New user confirmation",
-          text: "Welcome to Blocipedia!",
-          html:
-            "<strong>Welcome to Blocipedia and a new world of Wikis</strong>"
-        };
-        sgMail.send(msg);
+        //  const msg = {
+        //    to: newUser.email,
+        //    from: "test@example.com",
+        //    subject: "New user confirmation",
+        //    text: "Welcome to Blocipedia!",
+        //    html:
+        //      "<strong>Welcome to Blocipedia and a new world of Wikis</strong>"
+        //  };
+        //  sgMail.send(msg);
         callback(null, user);
       })
       .catch(err => {
