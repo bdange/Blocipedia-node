b7c6351497446521fec3bb34d63cb01a2a8ecbf5
bdange <benjamin.dange@gmail.com>
2019-08-15 00:41:10 +0200

    troubleshooting collabs

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -68,6 +68,7 @@ module.exports = {
           .findAll()
           .then(collaborations => {
             result["collaborations"] = collaborations;
+            console.log(collaborations);
             callback(null, result);
           })
           .catch(err => {
